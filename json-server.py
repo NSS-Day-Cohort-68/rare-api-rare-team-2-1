a423fcc34efa9712d6e90487e8e7d8fe0ebe48d7
Gavin Kashmark <gkashmark@gmail.com>
2024-03-05 15:23:21 -0600

    added initial backend methods and server comms for users

--- json-server.py
+++ json-server.py
@@ -1,92 +1,87 @@
 import json
 from http.server import HTTPServer
 from nss_handler import HandleRequests, status
-
-from views import get_all_orders, get_single_order, create_order, delete_order
-from views import update_metal, get_all_metals
+from views import get_all_users, retrieve_user
 
 
 class JSONServer(HandleRequests):
+
     def do_GET(self):
         response_body = ""
         url = self.parse_url(self.path)
 
-        if url["requested_resource"] == "orders":
+        if url["requested_resource"] == "users":
             if url["pk"] != 0:
-                response_body = get_single_order(url["pk"])
+                response_body = retrieve_user(url["pk"])
                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
-            response_body = get_all_orders()
-            return self.response(response_body, status.HTTP_200_SUCCESS.value)
-
-        elif url["requested_resource"] == "metals":
-            response_body = get_all_metals()
+            response_body = get_all_users()
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
         return self.response("", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
 
-    def do_POST(self):
-        url = self.parse_url(self.path)
-        content_len = int(self.headers.get("content-length", 0))
-        request_body = self.rfile.read(content_len)
-        request_body = json.loads(request_body)
+    # def do_POST(self):
+    #     url = self.parse_url(self.path)
+    #     content_len = int(self.headers.get("content-length", 0))
+    #     request_body = self.rfile.read(content_len)
+    #     request_body = json.loads(request_body)
 
-        if url["requested_resource"] == "orders":
-            successfully_posted = create_order(request_body)
-            if successfully_posted:
-                return self.response("", status.HTTP_201_SUCCESS_CREATED.value)
+    #     if url["requested_resource"] == "users":
+    #         successfully_posted = create_user(request_body)
+    #         if successfully_posted:
+    #             return self.response("", status.HTTP_201_SUCCESS_CREATED.value)
 
-        else:
-            return self.response(
-                "Not Found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
-            )
+    #     else:
+    #         return self.response(
+    #             "Not Found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+    #         )
 
-    def do_DELETE(self):
-        url = self.parse_url(self.path)
-        pk = url["pk"]
+    # def do_DELETE(self):
+    #     url = self.parse_url(self.path)
+    #     pk = url["pk"]
 
-        if url["requested_resource"] == "orders":
-            if pk != 0:
-                successfully_deleted = delete_order(pk)
-                if successfully_deleted:
-                    return self.response(
-                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
-                    )
+    #     if url["requested_resource"] == "users":
+    #         if pk != 0:
+    #             successfully_deleted = delete_user(pk)
+    #             if successfully_deleted:
+    #                 return self.response(
+    #                     "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+    #                 )
 
-                return self.response(
-                    "Requested resource not found",
-                    status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-                )
-            else:
-                return self.response(
-                    "Not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
-                )
+    #             return self.response(
+    #                 "Requested resource not found",
+    #                 status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+    #             )
+    #         else:
+    #             return self.response(
+    #                 "Not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+    #             )
 
-    def do_PUT(self):
-        url = self.parse_url(self.path)
-        pk = url["pk"]
+    # def do_PUT(self):
+    #     url = self.parse_url(self.path)
+    #     pk = url["pk"]
 
-        content_len = int(self.headers.get("content-length", 0))
-        request_body = self.rfile.read(content_len)
-        request_body = json.loads(request_body)
+    #     content_len = int(self.headers.get("content-length", 0))
+    #     request_body = self.rfile.read(content_len)
+    #     request_body = json.loads(request_body)
 
-        if url["requested_resource"] == "metals":
-            if "metals" and "price" in request_body:
-                if pk != 0:
-                    successfully_updated = update_metal(pk, request_body)
-                    if successfully_updated:
-                        return self.response(
-                            "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
-                        )
-            else:
-                return self.response(
-                    "Missing required keys in the request body",
-                    status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value,
-                )
-        return self.response(
-            "Requested resource not found",
-            status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
-        )
+    #     if url["requested_resource"] == "users":
+    #         if "metals" and "price" in request_body:
+    #             if pk != 0:
+    #                 successfully_updated = update_user(pk, request_body)
+    #                 if successfully_updated:
+    #                     return self.response(
+    #                         "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+    #                     )
+    #         else:
+    #             return self.response(
+    #                 "Missing required keys in the request body",
+    #                 status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value,
+    #             )
+    #     return self.response(
+    #         "Requested resource not found",
+    #         status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+    #     )
 
 
 def main():
