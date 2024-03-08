dbd57a265e2ca7073add8f3d9642084fd33193d5
Jean Patimeteeporn <jpatimet@gmail.com>
2024-03-08 14:00:21 -0600

    added missing import

--- views/__init__.py
+++ views/__init__.py
@@ -4,6 +4,7 @@ from .user import (
     create_user,
     login_user,
     retrieve_user_by_username,
+    retrieve_user_by_email,
 )
 from .post import get_all_posts, get_posts_by_user_id, get_single_post
 from .category import create_category, get_all_categories, delete_category
